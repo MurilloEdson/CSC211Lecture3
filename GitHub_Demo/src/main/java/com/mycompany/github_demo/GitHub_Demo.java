0964968b41a5b6c5ab57f557cbfcbd57fbc910c2
murilloedsonj@gmail.com <itlabs@WHB019_18.it.farmingdale.edu>
2021-09-09 14:29:17 -0400

    We have created a one dimension array

--- GitHub_Demo/src/main/java/com/mycompany/github_demo/GitHub_Demo.java
+++ GitHub_Demo/src/main/java/com/mycompany/github_demo/GitHub_Demo.java
@@ -5,6 +5,12 @@ public class GitHub_Demo
 {
     public static void main(String[] args) 
     {
- 
+        int[] arr= new int [5];
+        arr[0] = 5;
+        arr[2] =10;
+        for (int i = 0; i < 5; i++) 
+        {
+            System.out.println("Array element value: " + arr[i]);
+        }
     }
 }
